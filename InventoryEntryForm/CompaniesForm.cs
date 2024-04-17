3d3ecf7b6fe547b0b84be9d9bf3c06509d231af2
Sachin Chhetri <spaudelchhetri@mail.weber.edu>
2024-04-17 00:20:41 -0600

    first commit

---
+++ InventoryEntryForm/CompaniesForm.cs
@@ -0,0 +1,112 @@
+﻿using DataAccessLayer;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace InventoryEntryForm
+{
+    public partial class CompaniesForm : Form
+    {
+        public CompaniesForm()
+        {
+            InitializeComponent();
+
+            #region Data Grid Load
+            // loads the data into the datagrid
+            try
+            {
+                dgCompanies.DataSource = Utility.GetCompany();
+                //Hide ID column
+                dgCompanies.Columns["ID"].Visible = false;
+                DataGridViewButtonColumn deleteColumn = new DataGridViewButtonColumn();
+                deleteColumn.HeaderText = "Use to Delete";
+                deleteColumn.Text = "Delete";
+                deleteColumn.Name = "deleteButton";
+                deleteColumn.UseColumnTextForButtonValue = true;
+                dgCompanies.Columns.Add(deleteColumn);
+
+                DataGridViewButtonColumn updateColumn = new DataGridViewButtonColumn();
+                updateColumn.HeaderText = "Use to Update";
+                updateColumn.Text = "Update";
+                updateColumn.Name = "updateButton";
+                updateColumn.UseColumnTextForButtonValue = true;
+
+                dgCompanies.Columns.Add(updateColumn);
+            }
+            catch (SqlException SqlException)
+            {
+                MessageBox.Show("SQL specific error. Cannot Load Company. Contact your administrator.\n\nPhone Number: 801-767-9899, or Email Address: maiwau@super.com\n" + SqlException.Message);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Unhandled Exception. Contact your administrator.\n\nPhone Number: 801-767-9899, or Email Address\n" + ex.Message);
+            }
+
+            #endregion
+        }
+
+        private void CompaniesForm_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void RefreshCompanyDataGrid()
+        {
+            StoreManagement.CompanyDataTable dtCompanyTable = Utility.GetCompany();
+            dgCompanies.DataSource = dtCompanyTable;
+        }
+
+        private void dgCompanies_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+            int currentCompanyId = -1;
+
+            DataGridView dg = (DataGridView)sender;
+            if(e.RowIndex < 0)
+            {
+                return;
+            }
+
+            DataGridViewRow rowToBeOperatedUpon = dg.Rows[e.RowIndex];
+
+            currentCompanyId = int.Parse(rowToBeOperatedUpon.Cells["ID"].Value.ToString());
+
+            if (e.ColumnIndex == -1)
+            {
+                RefreshCompanyDataGrid();
+            }
+
+            if(dg.SelectedCells.Count == 1) 
+            {
+                if (dg.SelectedCells[0] is DataGridViewButtonCell)
+                {
+                    DataGridViewButtonCell selectedCell = (DataGridViewButtonCell)dg.SelectedCells[0];
+                    if (selectedCell.Value.Equals("Delete"))
+                    {
+                        Utility.DeleteCompaniesById(currentCompanyId);
+                        MessageBox.Show("Item Deleted!");
+
+                        RefreshCompanyDataGrid();
+                    }
+                    else if (selectedCell.Value.Equals("Update"))
+                    {
+                        string CompanyName = rowToBeOperatedUpon.Cells["CompanyName"].Value.ToString();
+                        string ContactNumber = rowToBeOperatedUpon.Cells["ContactNumber"].Value.ToString();
+                        string address = rowToBeOperatedUpon.Cells["Address"].Value.ToString();
+                        Utility.UpdateCompanies(currentCompanyId, CompanyName, ContactNumber, address);
+                        MessageBox.Show("Sucessfully Updated!", "Close Window", MessageBoxButtons.OK);
+                        RefreshCompanyDataGrid();
+                    }
+                }
+                return;
+            }
+        }
+    }
+}
---
+++ InventoryEntryForm/MainWindow.Designer.cs
@@ -0,0 +1,95 @@
+﻿namespace InventoryEntryForm
+{
+    partial class MainWindow
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.windowToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.inventoryToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.companiesToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.windowToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(1027, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // windowToolStripMenuItem
+            // 
+            this.windowToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.inventoryToolStripMenuItem,
+            this.companiesToolStripMenuItem});
+            this.windowToolStripMenuItem.Name = "windowToolStripMenuItem";
+            this.windowToolStripMenuItem.Size = new System.Drawing.Size(63, 20);
+            this.windowToolStripMenuItem.Text = "Window";
+            // 
+            // inventoryToolStripMenuItem
+            // 
+            this.inventoryToolStripMenuItem.Name = "inventoryToolStripMenuItem";
+            this.inventoryToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.inventoryToolStripMenuItem.Text = "Inventory";
+            this.inventoryToolStripMenuItem.Click += new System.EventHandler(this.inventoryToolStripMenuItem_Click);
+            // 
+            // companiesToolStripMenuItem
+            // 
+            this.companiesToolStripMenuItem.Name = "companiesToolStripMenuItem";
+            this.companiesToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.companiesToolStripMenuItem.Text = "Companies";
+            this.companiesToolStripMenuItem.Click += new System.EventHandler(this.companiesToolStripMenuItem_Click);
+            // 
+            // MainWindow
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1027, 904);
+            this.Controls.Add(this.menuStrip1);
+            this.IsMdiContainer = true;
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "MainWindow";
+            this.Text = "Simple Store System (SSS)";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem windowToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem inventoryToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem companiesToolStripMenuItem;
+    }
+}
